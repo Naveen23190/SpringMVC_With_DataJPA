8fbff1dc30a42534dce990ad43c9cbb3d617f4fe
M S Naveen Kumar <kumarnaven590@gmail.com>
2026-09-13 12:02:57 +0530

    Add files via upload

---
+++ SpringMVC_With_DataJPA/src/main/java/com/spring/controller/HomeController.java
@@ -0,0 +1,75 @@
+package com.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.entity.Student;
+import com.spring.repository.StudentRepository;
+import com.spring.service.StudentService;
+import com.spring.service.StudentServiceImpl;
+
+@Controller // -> It Will Return .jsp file
+//@RestController -> It Will Return JSON DATA
+public class HomeController 
+{
+	Student student = null;
+	@Autowired
+	private StudentService studentService;
+
+	@RequestMapping("/")
+	public String home() {
+		return "homePage";
+	}
+	
+//This Return JSP File , So We Have To Use @Controler
+	@RequestMapping("/save")
+	public String save(Model model) {
+		student = new Student();
+		student.setName("Naveen");
+		student.setAge(25);
+		Student saveStudent = studentService.saveStudent(student);
+		model.addAttribute("data",saveStudent);		
+		return "insertPage";
+		// return studentService.saveStudent(student) -> it will give the saved data to
+		// the browser // it Will Return The JSON data So We Have To Use @RestController
+	}
+	
+	
+//	@PostMapping("/save")
+//	public String save(@RequestBody Student student) 
+//	{
+//		studentService.saveStudent(student);
+//		return "Successfully Saved";
+//	}
+	// Use Postman To Send The Data "https://localhost:8080/save" use the URL and mention the post type and 
+	// `{"name" : "Rani" , "age" : 22}` and Click On Send ->   It Will Directly Store Into The Table
+	@RequestMapping("/update")
+	public String update() 
+	{
+		student.setName("M S Naveen Kumar");
+		studentService.updateStudent(student);
+		return "Successfully Updated";
+	}
+
+	@RequestMapping("/find/{id}")
+	public Student find(@PathVariable Long id) {
+		return studentService.findById(id);
+	}
+
+	@RequestMapping("/findAll")
+	public String findAll(Model model) 
+	{
+		List<Student> all = studentService.findAll();
+		model.addAttribute("student",all);
+		return "findAll";
+		
+	}
+}
---
+++ SpringMVC_With_DataJPA/src/main/webapp/META-INF/Views/insertPage.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert Page</title>
+</head>
+<body>
+  <h1>${data} Has Been Inserted Successfully</h1>
+</body>
+</html>
